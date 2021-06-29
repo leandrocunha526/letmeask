a9a0141272ea9c0d574ab0c4678b8328d7e7bf31
leandrocunha526 <leandrocunha016@gmail.com>
2021-06-29 20:29:10 -0300

    Import pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { AdminRoom } from './pages/AdminRoom';
+import { Room } from './pages/Room';
 import { NewRoom } from "./pages/NewRoom";
-import { Room } from "./pages/Room";
-import { AdminRoom } from "./pages/AdminRoom";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
 
