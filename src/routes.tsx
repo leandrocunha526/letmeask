3e67f131292cadb38dd3d59a39171e5a8757e10b
leandrocunha526 <leandrocunha016@gmail.com>
2021-06-29 18:53:41 -0300

    Update routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,29 @@ import React from "react";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 const Routes: React.FC = () => {
-	return(
-	  <BrowserRouter>
-	  <Switch>
-	  
-	  </Switch>
-	  </BrowserRouter>
-	)
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <AuthContextProvider>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+            <Route path="/rooms/:id" component={Room} />
+
+            <Route path="/admin/rooms/:id" component={AdminRoom} />
+          </Switch>
+        </AuthContextProvider>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
