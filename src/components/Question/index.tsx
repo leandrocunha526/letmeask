0d3fabb94575b2159ab4502f3c325b2c8b1ec500
leandrocunha526 <leandrocunha016@gmail.com>
2021-06-29 18:49:51 -0300

    Update component

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,44 +1,42 @@
-import {ReactNode} from 'react';
-import cx from 'classnames';
+import { ReactNode } from "react";
+import cx from "classnames";
 
-import './styles.scss';
+import "./styles.scss";
 
 type QuestionProps = {
-	content: string,
-	author: {
-		name: string,
-		avatar: string
-	};
-	children?: ReactNode;
-	isAnswered?: boolean;
-	isHighlighted?: boolean;
-}
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+};
 
 export function Question({
-	content,
-	author,
-	isAnswered = false,
-	isHighlighted = false,
-	children,
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
 }: QuestionProps) {
-	return(
-		<div 
-		className={cx(
-		  'question', 
-		  { answered: isAnswered },
-		  { highlighted: isHighlighted && !isAnswered },
-		)}
-	  >
-		<p>{content}</p>
-		<footer>
-		<div className="user-info">
+  return (
+    <div
+      className={cx(
+        "question",
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+    >
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div>
-          {children}
-        </div>
-		</footer>
-		</div>
-	)
+        <div>{children}</div>
+      </footer>
+    </div>
+  );
 }
